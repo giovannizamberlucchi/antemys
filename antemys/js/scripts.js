a22936d814e4e95452e59a53866962600d5299f9
giovannizamberlucchi <giovanni.zamberlucchi@gmail.com>
2023-10-26 08:55:47 +0200

    aterminer

--- antemys/js/scripts.js
+++ antemys/js/scripts.js
@@ -1,54 +1,6 @@
 (function ($) {
   $(document).ready(function () {
     "use strict";
-    // text
-    let i = 0;
-
-    const randomizeText = () => {
-    const phrase = document.querySelector('.random-word');
-    const compStyles = window.getComputedStyle(phrase);
-    const animation = compStyles.getPropertyValue('animation');
-    const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
-    
-    const phrases = ['energetic', 'cool', 'amazing', 'l33t', 'smart', 'strong', 'fierce', 'unstoppable', 'powerful', 'exciting', 'relentless', 'loving', 'kind', 'beast-mode', 'hardcore', 'intelligent', 'next-level', 'fun', 'free', 'awesome'];
-    
-    i = randomNum(i, phrases.length);
-    const newPhrase = phrases[i];
-    
-    setTimeout(() => {
-      phrase.textContent = newPhrase;
-    }, 400); // time to allow opacity to hit 0 before changing word
-    }
-    
-    const randomNum = (num, max) => {
-    let j = Math.floor(Math.random() * max);
-    
-    // ensure diff num every time
-    if (num === j) {
-      return randomNum(i, max);
-    } else {
-      return j;
-    }
-    }
-    
-    const getAnimationTime = () => {
-    const phrase = document.querySelector('.random-word');
-    const compStyles = window.getComputedStyle(phrase);
-    let animation = compStyles.getPropertyValue('animation');
-    
-    // firefox support for non-shorthand property
-    animation != "" ? animation : animation = compStyles.getPropertyValue('-moz-animation-duration');
-    
-      // webkit support for non-shorthand property
-    animation != "" ? animation : animation = compStyles.getPropertyValue('-webkit-animation-duration');
-    
-    
-    const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
-    return animationTime;
-    }
-    
-    randomizeText();
-    setInterval(randomizeText, getAnimationTime());
     
 
     // BACK BUTTON RELOAD
